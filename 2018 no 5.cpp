9465eb69f0492d4b4f56bbc3b817243ad3aee003
Bagus Wijaya <145143718+Baguswijaya1@users.noreply.github.com>
2023-10-09 15:33:58 +0700

    Update 2018 no 5.cpp
    edited

--- 2018 no 5.cpp
+++ 2018 no 5.cpp
@@ -5,20 +5,25 @@ int main(){
 	int jumlah_data;
 	cin>>jumlah_data;
 	
+	//input
 	int data[jumlah_data];
 	for (int a = 0; a<jumlah_data; a++){
 		cin>>data[a];
 	}
+
+	
+	//menentukan nilai tertinggi 
 	int output = 0;
 	int tertinggi = data[0];
 	for (int b = 0; b<jumlah_data; b++){
 		if (tertinggi <= data[b]){
 			tertinggi = data[b];
-			if (tertinggi == data[b]){
-				output ++;
-			}
-				
-			
+		}
+	}
+	//menentukan jumlah dari nilai tertinggi
+	for (int c = 0; c<jumlah_data; c++){
+		if (data[c]==tertinggi){
+			output++;
 		}
 	}
 	
